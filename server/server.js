1b9b7aecba957ae354e1624719c28bf4dc66732e
Amelia Abobo <aabobo@udel.edu>
2020-04-30 16:49:43 -0400

    working on file upload button

--- server/server.js
+++ server/server.js
@@ -17,3 +17,23 @@ app.get('/containers', (req, res) => {
 
 
 app.listen(2020, () => console.log("Listening"));
+
+//file upload button
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function(){
+	realFileBtn.click();
+});
+/*
+realFileBtn.addEventListener("change", function(){
+	if(realFileBtn.value) {
+		customTxt.innerHTML = realFileBtn.value.match(
+			/[\/\\]([\w\d\s\.\-\(\)]+)$/
+		)[1];
+	} else{
+		customTxt.innerHTML = "No file chosen, yet.";
+	}
+});
+*/
