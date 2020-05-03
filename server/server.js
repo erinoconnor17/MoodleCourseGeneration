b9036d7c12cbba22d3d76ffd6a3eb6d702d6cf75
Dodobirds <01010011c@gmail.com>
2020-05-03 19:48:02 +0000

    server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express()
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 
+var root_path = "/home/chenstev/root/";
 
 // Need to discuss what this sends
 app.get('/containers', (req, res) => {
@@ -15,5 +16,41 @@ app.get('/containers', (req, res) => {
 
 });
 
+let makeSession = (name, port) => {
+	let id = name + '_' + port;
+	let net_name = 'net_' + id;
+	let db_name = 'db_' + id;
+	let server_name = 'server_' + id;
 
+	let course_path = root_path + name + '/course';
+	// Network
+	docker.createNetwork({name : net_name})
+
+	.then(function(network) {
+		return docker.createContainer({Image: 'moodle_db:0.1', 
+					name: db_name, 
+					HostConfig: {NetworkMode: net_name}
+					});
+	})
+	.then(function(container) {
+		container.start();
+		return docker.createContainer({Image: 'moodle_server:0.1', 
+					name: server_name, 
+					Env: ["MARIADB_HOST="+db_name], 
+					HostConfig: {
+						NetworkMode: net_name ,
+						PortBindings: {"80/tcp": [{ "HostPort": port}]},
+						Binds: [ `${course_path}:/course` ]
+					}});
+	})
+	.then( (container) => container.start())
+	.catch( (e) => console.log(e));
+};
+
+app.get('/create', (req, res) => {
+	let name = "course_test";
+	let port = "8090";
+	makeSession(name, port);
+	docker.getContainer('server_'+name+'_'+port).inspect( (err, data) => res.send(data));
+});
 app.listen(2020, () => console.log("Listening"));
